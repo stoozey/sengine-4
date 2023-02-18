f3ff4d826a500d063b8a62f83880d2946ca4d667
stoozey_ <stoozey123@gmail.com>
2023-02-18 18:45:17 +0000

    added engine module

---
+++ src/engine/mod.rs
@@ -0,0 +1,57 @@
+use sdl2::video::Window;
+use sdl2::render::WindowCanvas;
+use sdl2::Sdl;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+
+pub mod engine;
+
+pub struct Engine {
+    context: Sdl,
+    window: Window,
+    canvas: WindowCanvas,
+    loop_running: bool,
+}
+
+impl Engine {
+    pub fn create() -> Result<Engine, String> {
+        let context = sdl2::init()?;
+        let video_subsystem = context.video()?;
+        let window = video_subsystem.window("Penis!", 1280, 720)
+            .position_centered()
+            .build()
+            .unwrap();
+
+        let canvas = window.into_canvas()
+            .build()
+            .unwrap();
+
+        Ok(Engine { context, window, canvas, loop_running: false })
+    }
+
+    pub fn run_loop(&mut self) {
+        loop_running = true;
+
+        let mut event_queue = context.event_pump().unwrap();
+        while loopRunning {
+            for event in event_queue.poll_iter() {
+                match event {
+                    Event::Quit {..} => {
+                        loop_running = false;
+                    },
+
+                    _=> { }
+                }
+            }
+
+            // TODO add update loop
+
+            canvas.set_draw_color(Color::RGB(255, 255, 255));
+            canvas.clear();
+
+            // TODO add render loop
+
+            canvas.present();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,11 @@
 use sdl2::pixels::Color;
-use sdl2::rect::Rect;
-use sdl2::{event, EventPump};
 use sdl2::event::Event;
 
+mod engine;
+
 fn main() -> Result<(), String> {
-    let context = sdl2::init()?;
-    let video_subsystem = context.video()?;
-    let window = video_subsystem.window("Penis!", 1280, 720)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas()
-        .build()
-        .unwrap();
-
-    let mut running = true;
-    let mut event_queue = context.event_pump().unwrap();
-
-    let mut x = 0.0;
-    while running {
-        for event in event_queue.poll_iter() {
-            match event {
-                Event::Quit {..} => {
-                    running = false;
-                },
-
-                _=> { }
-            }
-        }
-
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
-        canvas.clear();
-
-        let rect = Rect::new(x as i32, 0, 64, 64);
-        x += 0.05;
-
-        canvas.set_draw_color(Color::RGB(255, 0, 0));
-        canvas.fill_rect(rect)?;
-        canvas.present();
-    }
+    let mut engine = engine::Engine::create()?;
+    engine.run_loop();
 
     Ok(())
 }
